d8e18235b84b84a59e4afdea274bc071412550c3
werejoel <joelbank481@gmail.com>
2025-11-30 16:30:07 +0300

    vbvv

--- IdentityDemo/obj/Debug/net8.0/TenantsManagementApp.AssemblyInfo.cs
+++ IdentityDemo/obj/Debug/net8.0/TenantsManagementApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TenantsManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+446b2ea4fb70e3f8dafd5025d5f6c4d57c2793e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9e5baf57f3026fd3b6b91cdcd19c062be74e76")]
 [assembly: System.Reflection.AssemblyProductAttribute("TenantsManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TenantsManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
